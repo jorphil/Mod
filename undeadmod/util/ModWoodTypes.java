836804ed17e8a9498e5e7a2f1a98717d0636f7a9
jorphil <coffeeinanebula@gmail.com>
2025-05-21 18:32:36 +0200

    Delete undeadmod directory

--- undeadmod/util/ModWoodTypes.java
+++
@@ -1,9 +0,0 @@
-package net.diverginglensestudios.undeadmod.util;
-
-import net.diverginglensestudios.undeadmod.UndeadMod;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
-import net.minecraft.world.level.block.state.properties.WoodType;
-
-public class ModWoodTypes {
-    public static final WoodType ASH = WoodType.register(new WoodType(UndeadMod.MOD_ID + ":ash", BlockSetType.OAK));
-}
